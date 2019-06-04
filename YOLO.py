6d36e6857a5ecc39e2351d7a9634d96f2f6e9c71
Titi <a2699560@gmail.com>
2019-06-04 23:21:42 +0800

    6/4 upload

--- YOLO.py
+++ YOLO.py
@@ -32,6 +32,6 @@ if __name__ == "__main__":
 	
 	print("results:", results)
 	cv2.imwrite("results.jpg", img)
-	#cv2.imshow("image", img)
-	#cv2.waitKey(0)
-	#cv2.destroyAllWindows()
+	cv2.imshow("image", img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
--- yolo.py
+++ yolo.py
@@ -7,12 +7,13 @@ import time
 class Yolo():
 	def __init__(self, ip="192.168.0.177", port=8080):
 		##### YOLO #####
-		self.yolo = pydarknetYOLO(obdata="/home/titi/darknet/cfg/coco.data", weights="/home/titi/darknet/yolov3-tiny.weights", cfg="/home/titi/darknet/cfg/yolov3-tiny.cfg")
+		self.yolo = pydarknetYOLO(obdata="/home/titi/darknet/cfg/coco.data", weights="/home/titi/darknet/yolov3.weights", cfg="/home/titi/darknet/cfg/yolov3.cfg")
 		self.results = {
 			'labels': [],
 			'scores': [],
 			'middleX': [],
-			'middleY': []
+			'middleY': [],
+			'area': []
 		}
 
 		##### Socket server #####
@@ -30,11 +31,9 @@ class Yolo():
 		##### Write image #####
 		imgFile = open("socketImage.jpg", 'wb')
 		while True:
-			#print("enter while")
 			imgData = client.recv(1024)
 			if not imgData:
 				break
-			#print("write over")
 			imgFile.write(imgData)
 		imgFile.close()
 		print("image save")
@@ -50,23 +49,50 @@ class Yolo():
 			left, top, width, height, label, score = self.yolo.list_Label(i)
 			middle_x = left + width/2
 			middle_y = top + height/2
+			Area = width * height
 			self.results['labels'].append(label)
 			self.results['scores'].append(score)
 			self.results['middleX'].append(middle_x)
-			self.results['middleY'].append(middle_y)	
-		print("Object counts:", self.yolo.objCounts)
+			self.results['middleY'].append(middle_y)
+			self.results['area'].append(Area)
+		print("Labels:", self.results['labels'])
+		print("middleX:", self.results['middleX'])
+		print("middleY", self.results['middleY'])
+		print("area", self.results['area'])
 		cv2.imwrite("results.jpg", img)
 
-	#def target():
-	#		if 
+	def target(self):
+		personScore = []
+		personIndex = []
+		for index, name in enumerate(self.results['labels']):
+			if name == "person":
+				personIndex.append(index)
+				personScore.append(self.results['scores'][index])
+		if not personScore == []:
+			maxIndex = personScore.index(max(personScore))
+			Index = personIndex[0]
+			return (self.results['middleX'][Index], self.results['middleY'][Index], self.results['area'][Index])
+		else:
+			return (None, None, None)
 
-	def send_results(self):
+	def send_results(self, X, Y, A):   
 		HOST = "192.168.0.145"
 		PORT = 5050
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((HOST, PORT))
-		s.send(bytes("hello", encoding="utf8"))
+		pkg = "{} {} {}".format(X, Y, A)
+		s.send(bytes(pkg, encoding="utf8"))
 		s.close()
+		self.clean_results()
+
+	def clean_results(self):
+		self.results = {
+			'labels': [],
+			'scores': [],
+			'middleX': [],
+			'middleY': [],
+			'area': []
+		} 
 
 if __name__ == "__main__":
 	exe = Yolo(ip="192.168.0.177", port=8080)
@@ -75,5 +101,7 @@ if __name__ == "__main__":
 		exe.socket_server()
 		exe.yolo_detect()
 		time.sleep(1)
-		exe.send_results()
+		X, Y, A = exe.target()
+		time.sleep(1)
+		exe.send_results(X, Y, A)
 		time.sleep(1)
